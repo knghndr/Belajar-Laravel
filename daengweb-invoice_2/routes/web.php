de9ce4231ceed15b59c9f1d532d61c88bad57aef
knghndr <48hendra@gmail.com>
2019-05-01 18:10:07 +0700

    first commit

---
+++ daengweb-invoice_2/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'product'], function() {
+    Route::get('/', 'ProductController@index');
+    Route::get('/new', 'ProductController@create');
+    Route::post('/', 'ProductController@save');
+    Route::get('/{id}', 'ProductController@edit');
+    Route::put('/{id}', 'ProductController@update');
+    Route::delete('/{id}', 'ProductController@destroy');
+    
+});
